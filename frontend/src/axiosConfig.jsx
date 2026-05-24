8c2f3f0b7e01c1b4943a9659c7a7ada62bddeb53
peter.kok <peter.kok@connect.qut.edu.au>
2026-05-24 21:52:42 +0000

    Change axios base URL to use public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://localhost:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
-  //baseURL: 'http://3.26.130.245:5001', // live
+  baseURL: 'http://3.107.106.250:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
