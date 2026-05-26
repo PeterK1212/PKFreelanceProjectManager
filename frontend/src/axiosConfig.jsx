bf7c7c275e251ce4afc6ddbb155b7b142d452b43
peter.kok <peter.kok@connect.qut.edu.au>
2026-05-26 21:20:10 +0000

    Update axiosConfig.jsx to use current EC2 instance ID

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
-  baseURL: 'http://3.27.145.15:5001', // live
+  //baseURL: 'http://3.27.145.15:5001', // live
+  baseURL: 'http://3.26.196.206:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
